c3b3c6085a579822b40efc5e29e8881232dab060
Philipp Germann <philipp.germann@crg.eu>
2015-08-24 23:18:18 +0200

    Print language as title before listing versions

--- luke23/download.py
+++ luke23/download.py
@@ -2,24 +2,31 @@
 from lxml.html import parse
 
 
-versions = parse('versions.html')
+version_trs = parse('versions.html').xpath('//tr[@data-language]')
 
 
 def list_versions():
-    print('The following versions are available from BibleGateway:\n')
-    for link in versions.findall('//a'):
-        try:
+    print('The following versions are available from BibleGateway:')
+    for tr in version_trs:
+        for span in tr.findall('.//span[@class="language-display"]'):
+            print('\n' + span.text)
+        for link in tr.findall('.//a[@href]'):
             if link.attrib['href'].endswith('#booklist'):
                 print(link.text)
-        except KeyError:
-            pass
     print('\nTo download version use `$ python download.py version`.')
 
 
 def download(version):
+    print('Downloading ' + version)
     luke2 = parse('luke2.html').xpath('string()').split(version)[6]
     luke3 = parse('luke3.html').xpath('string()').split(version)[6]
     print(luke2, luke3)
+
+    for tr in version_trs:
+        if version in tr.xpath('string()'):
+            lang = tr.attrib['data-language']
+            break
+    print(lang + '-' + version)
 
 
 if __name__ == '__main__':
