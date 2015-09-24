4df64bcd0b557702d70d076fe5d72ae156f082d8
Philipp Germann <philipp.germann@crg.eu>
2015-09-24 23:35:53 +0200

    Test positivity

--- test_cluster.py
+++ test_cluster.py
@@ -7,6 +7,11 @@ from cluster import zipstance
 
 
 class TestZipstance(unittest.TestCase):
+
+    @given(text(), text())
+    def test_positivity(self, a, b):
+        self.assertTrue(zipstance(a, b) >= 0)
+
     @given(text(min_size=1), text(min_size=1), text(min_size=1))
     def test_triangle_inequality(self, a, b, c):
         self.assertTrue(zipstance(a, c) <= zipstance(a, b) + zipstance(b, c))
